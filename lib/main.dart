39a6ea94798490422fae0a85265d15dbdc594220
1be0nly <safari.firdaus04@gmail.com>
2023-01-05 18:03:56 +0700

    update1

--- lib/main.dart
+++ lib/main.dart
@@ -22,32 +22,35 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Users',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: Scaffold(
         appBar: AppBar(
           title: Text('Users'),
         ),
-        body: FutureBuilder<List<User>>(
-          future: futureUsers,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              List<User>? users = snapshot.data;
-              return ListView(
-                children: users!
-                    .map((user) => ListTile(
-                          title: Text(user.firstName + ' ' + user.lastName),
-                          subtitle: Text(user.email),
-                        ))
-                    .toList(),
-              );
-            } else if (snapshot.hasError) {
-              return Text('${snapshot.error}');
-            }
-            // By default, show a loading spinner.
-            return CircularProgressIndicator();
-          },
+        body: Container(
+          child: FutureBuilder<List<User>>(
+            future: futureUsers,
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              if (snapshot.hasData) {
+                List<User> users = snapshot.data;
+                return ListView.builder(
+                    itemCount: users.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return ListTile(
+                        leading: Image.network(users[index].image),
+                        title: Text(users[index].firstName +
+                            " " +
+                            users[index].lastName),
+                        subtitle: Text(users[index].email),
+                      );
+                    });
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
+              // By default, show a loading spinner.
+              return Center(child: CircularProgressIndicator());
+            },
+          ),
         ),
       ),
     );
@@ -55,12 +58,13 @@ class _MyAppState extends State<MyApp> {
 }
 
 Future<List<User>> fetchUsers() async {
-  final response = await http.get(Uri.parse('https://dummyjson.com/users'));
+  final response =
+      await http.get(Uri.parse('https://reqres.in/api/users?per_page=12'));
 
   if (response.statusCode == 200) {
     // If the server returns a 200 OK response, parse the JSON.
     Map<String, dynamic> parsedJson = jsonDecode(response.body);
-    List<dynamic> usersJson = parsedJson['users'];
+    List<dynamic> usersJson = parsedJson['data'];
     return usersJson.map((json) => User.fromJson(json)).toList();
   } else {
     // If the server returns a non-200 response, throw an error.
@@ -73,19 +77,22 @@ class User {
   final String firstName;
   final String lastName;
   final String email;
+  final String image;
 
   User(
       {required this.id,
       required this.firstName,
       required this.lastName,
-      required this.email});
+      required this.email,
+      required this.image});
 
   factory User.fromJson(Map<String, dynamic> json) {
     return User(
       id: json['id'],
-      firstName: json['firstName'],
-      lastName: json['lastName'],
+      firstName: json['first_name'],
+      lastName: json['last_name'],
       email: json['email'],
+      image: json['avatar'],
     );
   }
 }
